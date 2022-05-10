1d41ef3cc898d5bdbefc6fe22cad7a222f9ce41c
Nassim KISSI <nkissi@MacBook-Pro-de-Atecna-3.local>
2022-05-10 13:56:57 +0200

    @HostListener, @Input and 1st routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
-
+import { Component } from '@angular/core';
 /**
  * Un composant web = une classe + une vue
  */
@@ -9,23 +6,4 @@ import { Pokemon } from './pokemon';
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS
-  pokemonSelected: Pokemon|undefined; 
-
-  ngOnInit() {
-    console.table(this.pokemonList)
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-
-    if(pokemon){
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`)
-      this.pokemonSelected = pokemon
-    } else {
-      console.log(`Vous avez demandé un pokémon qui n'existe pas`)
-      this.pokemonSelected = pokemon
-    }
-  }
-}
+export class AppComponent {}
---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,38 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[pkmnBorderCard]'
+})
+export class BorderCardDirective {
+
+  // props 
+  private intialColor: string = '#f5f5f5'
+  private defaultColor: string = '#009688'
+  private defaultHeight: number = 180
+
+
+  constructor(private el: ElementRef) { 
+    this.setHeight(180)
+    this.setBorder('#f5f5f5')
+  }
+
+  @Input('pkmnBorderCard') borderColor: string; // alias
+
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder(this.borderColor || '#009688')
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5')
+  }
+
+  setHeight(height: number){
+    this.el.nativeElement.style.height = `${height}px`
+  }
+
+  setBorder(color: string){
+    this.el.nativeElement.style.border = `solid 4px ${color}`
+  }
+  
+}
